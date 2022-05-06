e7d78ded7634b7c8f90e14d5cdb976fe8762b147
fantomitechno <67735304+fantomitechno@users.noreply.github.com>
2022-05-06 20:29:52 +0200

    fix: rename custom_id

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -32,63 +32,73 @@ export default new Command(
         }
       }
     );
-    client.db.all(`SELECT * FROM start`, async (_err, row) => {
-      let menu: SelectMenu;
-      if (!row.length) {
-        menu = {
-          disabled: true,
-          custom_id: 'start_select',
-          placeholder: 'No starting point created for now',
-          type: 3,
-          options: [
-            {
-              value: 'nop',
-              label: 'No starting point'
-            }
-          ]
-        };
-      } else {
-        menu = {
-          custom_id: 'start_select',
-          placeholder: 'Select a starting point',
-          type: 3,
-          options: []
-        };
-        for (const r of row) {
-          await axios
-            .get(
-              `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${r.video}&key=${process.env.YOUTUBE_API_KEY}`
-            )
-            .then((res) => {
-              const title = res.data.items[0].snippet.title;
-              menu.options.push({
-                label: title,
-                value: r.id
-              });
-            });
-        }
-      }
-      client.interaction.editOriginalInteractionResponse(interaction.token, {
-        components: [
-          {
-            type: 1,
-            components: [menu]
-          },
-          {
-            type: 1,
-            components: [
+    client.db.all(
+      `SELECT * FROM start`,
+      async (
+        _err,
+        row: {
+          video: string;
+          description: string;
+        }[]
+      ) => {
+        let menu: SelectMenu;
+        if (!row.length) {
+          menu = {
+            disabled: true,
+            custom_id: 'start_select',
+            placeholder: 'No starting point created for now',
+            type: 3,
+            options: [
               {
-                type: 2,
-                label: 'Add a starting point',
-                custom_id: 'start_add',
-                style: 1
+                value: 'nop',
+                label: 'No starting point'
               }
             ]
+          };
+        } else {
+          menu = {
+            custom_id: 'start_choose',
+            placeholder: 'Select a starting point',
+            type: 3,
+            options: []
+          };
+          for (const r of row) {
+            await axios
+              .get(
+                `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${r.video}&key=${process.env.YOUTUBE_API_KEY}`
+              )
+              .then((res) => {
+                const title = res.data.items[0].snippet.title;
+                menu.options.push({
+                  label: title,
+                  value: r.video,
+                  description: r.description
+                });
+              });
           }
-        ],
-        content: 'Select a starting point'
-      });
-    });
+        }
+        client.interaction.editOriginalInteractionResponse(interaction.token, {
+          components: [
+            {
+              type: 1,
+              components: [menu]
+            },
+            {
+              type: 1,
+              components: [
+                {
+                  type: 2,
+                  label: 'Add a starting point',
+                  custom_id: 'create_start',
+                  style: 1
+                }
+              ]
+            }
+          ],
+          content: 'Select a starting point'
+        });
+      }
+    );
     return;
   }
 );
